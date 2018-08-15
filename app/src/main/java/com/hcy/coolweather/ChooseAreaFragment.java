3aee14fdeebdf74f61a1af52383e4b0030593ef6
simon <simon_hcy@163.com>
2018-08-15 22:18:04 +0800

    修复联网url的bug

--- app/src/main/java/com/hcy/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/hcy/coolweather/ChooseAreaFragment.java
@@ -164,7 +164,7 @@ public class ChooseAreaFragment extends Fragment {
             currentLevel = LEVEL_CITY;
         } else {
             int provinceCode = selectedProvince.getProvinceCode();
-            String address = "http://guolin.tech/api/china" + provinceCode;
+            String address = "http://guolin.tech/api/china/" + provinceCode;
             queryFromServer(address, "city");
         }
 
@@ -189,7 +189,7 @@ public class ChooseAreaFragment extends Fragment {
         } else {
             int provinceCode = selectedProvince.getProvinceCode();
             int cityCode = selectedCity.getCityCode();
-            String address = "http://guolin.tech/api/china" + provinceCode + "/" + cityCode;
+            String address = "http://guolin.tech/api/china/" + provinceCode + "/" + cityCode;
             queryFromServer(address, "county");
         }
     }
